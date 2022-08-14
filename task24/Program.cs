b7b167db1a75896b7e95c8d1dea6aac12f2dd9f5
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-14 11:08:13 +0300

    addTask

---
+++ task24/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+//int GetSumFrom1ToA(int numberA)
+//{
+  //  int sum = 0;
+    //for (int i = 1; i <= numberA; i++)
+    //{
+      //  sum += i;
+    //}
+    //return sum;
+//}
+
+int Getsum(int numberA)
+{
+    return numberA * (numberA + 1) / 2;
+}
+
+
+System.Console.WriteLine("Введите число");
+int numberA = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine(Getsum(numberA));
---
+++ task26/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 26: Напишите программу, которая принимает на 
+//вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+System.Console.Clear();
+
+int NumbersOfNumbers (int numberUser)
+{
+ int count = 0;
+ while (numberUser > 0)
+ {
+    numberUser /= 10;
+    count++;
+ }
+return count;
+}
+
+System.Console.WriteLine("Введите Ваше число");
+int numberUser = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine($"Вот количество цифр в вашем числе: {NumbersOfNumbers(numberUser)}");
